3a314fb1c3eef6bb061f05bdf8f3be02c2282118
Priyanshu Khullar <59757194+Priyanshu21101997@users.noreply.github.com>
2020-08-11 16:50:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ cv=pickle.load(open('transform.pkl','rb'))
 app = Flask(__name__)
 
 @app.route('/')
-def mian():
+def main():
 	return render_template('home.html')
 
 @app.route('/predict',methods=['POST'])
